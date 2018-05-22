20c474745dbc50031ed841c7f7697adcd3885ab6
rinikercody <rinikercody@gmail.com>
2018-05-22 16:25:45 -0500

    Number Game C#

---
+++ PersonalProjects/C#/Number Game/Number Game/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Number_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxComputerLabel = new System.Windows.Forms.Label();
+            this.uxPlayerLabel = new System.Windows.Forms.Label();
+            this.uxEasyButton = new System.Windows.Forms.RadioButton();
+            this.uxMediumButton = new System.Windows.Forms.RadioButton();
+            this.uxHardButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // uxComputerLabel
+            // 
+            this.uxComputerLabel.AutoSize = true;
+            this.uxComputerLabel.Location = new System.Drawing.Point(357, 88);
+            this.uxComputerLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.uxComputerLabel.Name = "uxComputerLabel";
+            this.uxComputerLabel.Size = new System.Drawing.Size(111, 44);
+            this.uxComputerLabel.TabIndex = 0;
+            this.uxComputerLabel.Text = "Enter";
+            // 
+            // uxPlayerLabel
+            // 
+            this.uxPlayerLabel.AutoSize = true;
+            this.uxPlayerLabel.Location = new System.Drawing.Point(66, 88);
+            this.uxPlayerLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.uxPlayerLabel.Name = "uxPlayerLabel";
+            this.uxPlayerLabel.Size = new System.Drawing.Size(117, 44);
+            this.uxPlayerLabel.TabIndex = 1;
+            this.uxPlayerLabel.Text = "Press";
+            // 
+            // uxEasyButton
+            // 
+            this.uxEasyButton.AutoSize = true;
+            this.uxEasyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxEasyButton.Location = new System.Drawing.Point(24, 25);
+            this.uxEasyButton.Name = "uxEasyButton";
+            this.uxEasyButton.Size = new System.Drawing.Size(77, 29);
+            this.uxEasyButton.TabIndex = 2;
+            this.uxEasyButton.TabStop = true;
+            this.uxEasyButton.Text = "Easy";
+            this.uxEasyButton.UseVisualStyleBackColor = true;
+            this.uxEasyButton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // uxMediumButton
+            // 
+            this.uxMediumButton.AutoSize = true;
+            this.uxMediumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxMediumButton.Location = new System.Drawing.Point(220, 25);
+            this.uxMediumButton.Name = "uxMediumButton";
+            this.uxMediumButton.Size = new System.Drawing.Size(103, 29);
+            this.uxMediumButton.TabIndex = 3;
+            this.uxMediumButton.TabStop = true;
+            this.uxMediumButton.Text = "Medium";
+            this.uxMediumButton.UseVisualStyleBackColor = true;
+            this.uxMediumButton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // uxHardButton
+            // 
+            this.uxHardButton.AutoSize = true;
+            this.uxHardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxHardButton.Location = new System.Drawing.Point(451, 25);
+            this.uxHardButton.Name = "uxHardButton";
+            this.uxHardButton.Size = new System.Drawing.Size(75, 29);
+            this.uxHardButton.TabIndex = 4;
+            this.uxHardButton.TabStop = true;
+            this.uxHardButton.Text = "Hard";
+            this.uxHardButton.UseVisualStyleBackColor = true;
+            this.uxHardButton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 236);
+            this.Controls.Add(this.uxHardButton);
+            this.Controls.Add(this.uxMediumButton);
+            this.Controls.Add(this.uxEasyButton);
+            this.Controls.Add(this.uxPlayerLabel);
+            this.Controls.Add(this.uxComputerLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "Form1";
+            this.Text = "Number Game";
+//            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label uxComputerLabel;
+        private System.Windows.Forms.Label uxPlayerLabel;
+        private System.Windows.Forms.RadioButton uxEasyButton;
+        private System.Windows.Forms.RadioButton uxMediumButton;
+        private System.Windows.Forms.RadioButton uxHardButton;
+    }
+}
+
---
+++ PersonalProjects/C#/Number Game/Number Game/Form1.cs
@@ -0,0 +1,168 @@
+﻿/* Author: Cody Riniker */
+/* This program is a game were the user will try to keep up with the computer's number on screen. */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Number_Game
+{
+    public partial class Form1 : Form
+    {
+        //The number the player is controlling
+        private int _playerNumber = 0;
+
+        //The oppenent computers number
+        private int _computerNumber = 0;
+
+        //Tick event is used to handle progression of computer player.
+        private Timer _timer = new Timer();
+
+        //Used to determine if the player has been out of range of the computers number for too long.
+        private Stopwatch _playerWatch = new Stopwatch();
+        private int _playerTime;
+
+        //Used to get random numbers for computer
+        private Random _random = new Random();
+
+        //The allowed difference between player's and computer's number
+        private int _distance;
+
+        //Controls the range of numbers the computer will increment/decrement by
+        private int _computerRange;
+
+        //Used to help manage speed of computer after the timers intervals get lower.
+        private Stopwatch _waitTimer = new Stopwatch();
+        private int _waitTime;
+
+        //What the computers speed increase by
+        private int _speedIncrease;
+
+        //Tracks players score
+        private Stopwatch _scoreClock = new Stopwatch();
+        private int _mulitplier;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _timer.Tick += TickEvent;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 'w')
+            {
+                _playerNumber++;
+                uxPlayerLabel.Text = _playerNumber.ToString();
+            }
+            if(e.KeyChar == 's')
+            {
+                _playerNumber--;
+                uxPlayerLabel.Text = _playerNumber.ToString(); 
+            }
+
+            //Start the game
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                if (uxEasyButton.Checked) //Easy mode
+                {
+                    _distance = 10;
+                    _speedIncrease = 100;
+                    _playerTime = 5000;
+                    _waitTime = 5000;
+                    _computerRange = 5;
+                    _mulitplier = 1;
+                }
+                else if (uxMediumButton.Checked) //Medium mode
+                {
+                    _distance = 7;
+                    _speedIncrease = 200;
+                    _playerTime = 3000;
+                    _waitTime = 3000;
+                    _computerRange = 10;
+                    _mulitplier = 3;
+                }
+                else //Hard mode
+                {
+                    _distance = 5;
+                    _speedIncrease = 300;
+                    _playerTime = 2000;
+                    _waitTime = 2000;
+                    _computerRange = 12;
+                    _mulitplier = 5;
+                }
+
+                //Setup
+                uxEasyButton.Hide();
+                uxMediumButton.Hide();
+                uxHardButton.Hide();
+                this.Text = "Keep Up With The Computer";
+                _playerNumber = 0;
+                _computerNumber = 0;
+                uxPlayerLabel.Text = "0";
+                uxComputerLabel.Text = "0";
+
+                //Start Game
+                _scoreClock.Start();
+                _waitTimer.Start();
+                _timer.Start();
+            }
+        }
+
+        //Executed after each interval of time by _timer
+        private void TickEvent(object sender, EventArgs e)
+        {
+            //Determines if player is out of range of computer
+            if(_computerNumber > _playerNumber + _distance || _computerNumber < _playerNumber - _distance)
+            {
+                _playerWatch.Start(); //Player is out of range and needs to get back in range
+            }
+            else
+            {
+                _playerWatch.Reset(); //Player is back in range so timer is reset
+            }
+
+            //If player has been out of range for too long, Game is over
+            if (_playerWatch.ElapsedMilliseconds >= _playerTime)
+            {
+                _timer.Stop();
+                _playerWatch.Stop();
+                MessageBox.Show("You lose\nYour Score Is:\n" + _scoreClock.ElapsedMilliseconds * _mulitplier);
+                
+                //Reset Game
+                _playerNumber = 0;
+                _computerNumber = 0;
+                uxEasyButton.Show();
+                uxMediumButton.Show();
+                uxHardButton.Show();
+            }
+
+            //Computer Player will now act
+            if (_waitTimer.ElapsedMilliseconds >= _waitTime)
+            {
+                _waitTimer.Reset();
+
+                //Gets a random number to add or substract from the computer's number
+                int num = _random.Next(_computerRange);
+                int sign = _random.Next(2);
+                if (sign == 0) _computerNumber -= num;
+                if (sign == 1) _computerNumber += num;
+                uxComputerLabel.Text = _computerNumber.ToString();
+
+                if(_waitTime > 700) //Make sure numbers don't change to fast
+                {
+                    //Computer will now change it's number faster
+                    _waitTime -= _speedIncrease;
+                }
+                _waitTimer.Start();
+            }
+        }
+
+    } //End class
+} //End namepace
